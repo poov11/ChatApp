c284e1cd5f95f8211f64866f8355aa9d9444f0b2
poov11 <poovarasan456789@gmail.com>
2023-11-23 04:00:32 +0530

    done decryption

--- php/get-chat.php
+++ php/get-chat.php
@@ -1,37 +1,50 @@
 <?php 
-    session_start();
-    if(isset($_SESSION['unique_id'])){
-        include_once "config.php";
-        $outgoing_id = $_SESSION['unique_id'];
-        $incoming_id = mysqli_real_escape_string($conn, $_POST['incoming_id']);
-        $output = "";
-        $sql = "SELECT * FROM messages LEFT JOIN users ON users.unique_id = messages.outgoing_msg_id
-                WHERE (outgoing_msg_id = {$outgoing_id} AND incoming_msg_id = {$incoming_id})
-                OR (outgoing_msg_id = {$incoming_id} AND incoming_msg_id = {$outgoing_id}) ORDER BY msg_id";
-        $query = mysqli_query($conn, $sql);
-        if(mysqli_num_rows($query) > 0){
-            while($row = mysqli_fetch_assoc($query)){
-                if($row['outgoing_msg_id'] === $outgoing_id){
-                    $output .= '<div class="chat outgoing">
+ session_start();
+if (isset($_SESSION['unique_id'])) {
+    include_once "config.php";
+    $strongKey = 'qkwjdiw239&&jdafweihbrhnan&^%$ggdnawhd4njshjwuuO';
+    $outgoing_id = $_SESSION['unique_id'];
+    $incoming_id = mysqli_real_escape_string($conn, $_POST['incoming_id']);
+    $output = "";
+
+    // Retrieve and decrypt messages from the database
+    $sql = "SELECT * FROM messages WHERE (outgoing_msg_id = {$outgoing_id} AND incoming_msg_id = {$incoming_id}) 
+            OR (outgoing_msg_id = {$incoming_id} AND incoming_msg_id = {$outgoing_id}) ORDER BY msg_id";
+
+    $query = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($query) > 0) {
+        while ($row = mysqli_fetch_assoc($query)) {
+            $encrypted_message = $row['msg'];
+            $iv = $row['iv'];
+
+            // Convert hex IV to binary
+            //$iv = hex2bin($hex_iv);
+
+            // Decrypt the message
+            $decrypted_message = openssl_decrypt($encrypted_message, 'aes-256-cbc', $strongKey, 0, $iv);
+
+            if ($row['outgoing_msg_id'] === $outgoing_id) {
+                $output .= '<div class="chat outgoing">
                                 <div class="details">
-                                    <p>'. $row['msg'] .'</p>
+                                    <p>' .$decrypted_message . '</p>
                                 </div>
-                                </div>';
-                }else{
-                    $output .= '<div class="chat incoming">
-                                <img src="php/images/'.$row['img'].'" alt="">
+                            </div>';
+            } else {
+                $output .= '<div class="chat incoming">
                                 <div class="details">
-                                    <p>'. $row['msg'] .'</p>
+                                    <p>' . $decrypted_message . '</p>
                                 </div>
-                                </div>';
-                }
+                            </div>';
             }
-        }else{
-            $output .= '<div class="text">No messages are available. Once you send message they will appear here.</div>';
         }
-        echo $output;
-    }else{
-        header("location: ../login.php");
+    } else {
+        $output .= '<div class="text">No messages are available. Once you send messages, they will appear here.</div>';
     }
 
+    echo $output;
+} else {
+    header("location: ../login.php");
+}
 ?>
+    
--- php/insert-chat.php
+++ php/insert-chat.php
@@ -1,16 +1,46 @@
   <?php 
-    session_start();
+    /*session_start();
     if(isset($_SESSION['unique_id'])){
         include_once "config.php";
         $outgoing_id = $_SESSION['unique_id'];
         $incoming_id = mysqli_real_escape_string($conn, $_POST['incoming_id']);
         $message = mysqli_real_escape_string($conn, $_POST['message']);
         if(!empty($message)){
-            message_hash = password_hash("$message",PASSWORD_BCRYPT);
+            message_hash = password_hash("$message",PASSWORD_DEFAULT);
              $sql = mysqli_query($conn, "INSERT INTO messages (incoming_msg_id, outgoing_msg_id, msg)
                                         VALUES ({$incoming_id}, {$outgoing_id}, '{$message_hash}')") or die();
         }
     }else{
         header("location: ../login.php");
     }
+
+    <?php*/
+session_start();
+
+if (isset($_SESSION['unique_id'])) {
+    include_once "config.php";
+    $strongKey = 'qkwjdiw239&&jdafweihbrhnan&^%$ggdnawhd4njshjwuuO';
+    $outgoing_id = $_SESSION['unique_id'];
+    $incoming_id = mysqli_real_escape_string($conn, $_POST['incoming_id']);
+    $output = "";
+    $message = mysqli_real_escape_string($conn, $_POST['message']);
+
+        // Encrypt the message
+        $iv = openssl_random_pseudo_bytes(16); // Use a fixed length of 16 bytes
+        $encrypted_message = openssl_encrypt($message, 'aes-256-cbc', $strongKey, 0, $iv);
+
+        if (!empty($encrypted_message)) {
+            // Insert the encrypted message and IV into the database
+//            $hex_iv = bin2hex($iv);
+            $sql = "INSERT INTO messages (incoming_msg_id, outgoing_msg_id, msg, iv)
+                    VALUES ({$incoming_id}, {$outgoing_id}, '{$encrypted_message}', '{$iv}')";
+            $query = mysqli_query($conn, $sql);
+
+            if (!$query) {
+                die(mysqli_error($conn));
+            }
+        }
+    }
+
+
 ?>
